d3adf794b902e898f5245587966bde0dd078b3c6
武继龙 <wujilong@58.com>
2024-04-12 17:46:57 +0800

    更新

--- EXP.py
+++ EXP.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import re
 # from models.BuildModel import crnnModel, SVTRModel
 # import yaml
 #
@@ -7,6 +10,58 @@
 #
 # print(model)
 
-a = []
-a += [1,2]
-print(a)
+
+def readlines(savedir):
+    os.makedirs(savedir, exist_ok=True)
+    srcdir = "/code/wangshiyuan02/data/ocr/commocr/textline/generateData/companyname700w40032"
+    txtpath = "/code/wangshiyuan02/data/ocr/commocr/textline/generateData/tmp_labels700w.txt"
+
+    picpaths = []
+    labels = []
+    count = 0
+    with open(txtpath, "r", encoding="utf-8") as f:
+        line = f.readline()
+        while line:
+            if "_" not in line:
+                line = f.readline()
+                continue
+            if count > 105:
+                break
+            count += 1
+            factors = line.strip().split()
+            picname = factors[0] + ".jpg"
+            label = factors[-1]
+            picpath = os.path.join(srcdir, picname)
+            if os.path.exists(picpath):
+                dstpath = os.path.join(savedir, label+".jpg")
+                shutil.copy(picpath, dstpath)
+                picpaths.append(picpath)
+                labels.append(label)
+            line = f.readline()
+    return picpaths, labels
+
+
+def calculate(txtpath):
+    with open(txtpath, "r") as f:
+        lines = f.readlines()
+    allnum = len(lines)
+    count = 0
+    for line in lines:
+        factors = line.strip().split("||")
+        keyname = os.path.split(factors[0])[-1][:-4]
+        # print(keyname)
+        label = factors[1]
+        label = re.sub(r"(限)+", "限", label)
+        label = re.sub(r"(责)+", "责", label)
+        label = re.sub(r"(任)+", "任", label)
+        label = label.replace("公公司", "公司")
+        if keyname == label:
+            count += 1
+    return count / allnum
+crnntxt = "/code/wujilong/code/OCR-TextRecog/crnn.txt"
+svtrtxt = "/code/wujilong/code/OCR-TextRecog/svtr.txt"
+
+crnnrecall = calculate(crnntxt)
+svtrrecall = calculate(svtrtxt)
+print("crnn {}".format(crnnrecall))
+print("svtr {}".format(svtrrecall))
--- infer.py
+++ infer.py
@@ -10,11 +10,12 @@ import shutil
 from models.BuildModel import crnnModel,SVTRModel
 from utils.label2tensor import strLabelConverter
 from models.backbones.repvggblock import repvgg_model_convert
-from utils.tools import loadkeyfile,create_module,makedir
+from utils.tools import loadkeyfile,create_module,makedir, loadchkeyfile
 from PIL import Image,ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import torchvision.transforms as transforms
 from utils.tools import tensor2img
+import time
 
 
 class CRNNInfer(object):
@@ -26,10 +27,10 @@ class CRNNInfer(object):
         elif config['train']['algorithm']=='SVTR':
             model = SVTRModel(config).cuda(config['train']['local_rank'])
         model_dict = torch.load(config['infer']['model_file'])['state_dict']
-        new_dict = {}
-        for key in model.state_dict().keys():
-            new_dict[key] = model_dict['module.'+key]
-        model.load_state_dict(new_dict)
+        # new_dict = {}
+        # for key in model.state_dict().keys():
+        #     new_dict[key] = model_dict['module.'+key]
+        model.load_state_dict(model_dict)
 #         self.model = repvgg_model_convert(model,None)
         self.model = model
         self.model.eval() 
@@ -37,21 +38,36 @@ class CRNNInfer(object):
         print(model.state_dict().keys())
         torch.save(model.state_dict(),'model.pth')
     def recogimg(self,img):
-        w, h = img.size
-#         new_w = int(32 / float(h) * w)//4*4
-#         if new_w>128:
-#             new_w = 128
-#         img = img.resize((new_w, 32), Image.BILINEAR)
-        img = img.resize((256, 48), Image.BILINEAR)
+        # w, h = img.size
+        # new_w = int(32 / float(h) * w)//4*4
+        # if new_w>128:
+        #     new_w = 128
+        # img = img.resize((new_w, 32), Image.BILINEAR)
+        img = img.resize((256, 32), Image.BILINEAR)
         if self.isGray:
             img = img.convert('L')
         img = transforms.ToTensor()(img)
         img.sub_(0.5).div_(0.5)
+        beg = time.time()
         with torch.no_grad():
             preds_class,preds_map,stn_x = self.model(img.unsqueeze(0).cuda())
+           # print(preds_class.size(), preds_map.size(), stn_x.size())
+            print(preds_class.size())
+        torch.cuda.synchronize()
+        end = time.time()
+        print("spendtime: {}".format(end-beg))
         cv2.imwrite('stn.jpg',tensor2img(stn_x[0]))
         preds_size = torch.IntTensor([preds_class.size(0)] * preds_class.size(1))
+        #print(preds_class)
+       # preds_class = torch.nn.functional.softmax(preds_class, dim=-1)
         _, preds_class = preds_class.max(2)
+        # dict = {}
+        # num = preds_class.size(0)
+        # for i in range(num):
+        #     score = _[i][0].item()
+        #     index = preds_class[i][0].item()
+        #     dict[index] = score
+        # print(dict)
         preds_class = preds_class.squeeze(1).contiguous().view(-1)
         sim_preds_char = self.converter.decode(preds_class.data, preds_size.data, raw=False)
         return sim_preds_char
@@ -66,6 +82,7 @@ class CRNNInferCPU(object):
             model = SVTRModel(config).to(config['train']['local_rank'])
         import pdb
         pdb.set_trace()
+        # 配置文件中的infer 选项
         model_dict = torch.load(config['infer']['model_file'],map_location='cpu')['state_dict']
         new_dict = {}
         for key in model.state_dict().keys():
@@ -82,7 +99,7 @@ class CRNNInferCPU(object):
 #         if new_w>128:
 #             new_w = 128
 #         img = img.resize((new_w, 32), Image.BILINEAR)
-        img = img.resize((256, 48), Image.BILINEAR)
+        img = img.resize((256, 32), Image.BILINEAR)
         if self.isGray:
             img = img.convert('L')
         img = transforms.ToTensor()(img)
@@ -102,20 +119,30 @@ class CRNNInferCPU(object):
         
 
 def demo(opt):
+    savetxt = "/code/wujilong/code/OCR-TextRecog/crnn.txt"
+    if os.path.exists(savetxt):
+        os.remove(savetxt)
+    #os.makedirs(savedir, exist_ok=True)
     path = opt.path
     os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpuid
     config = yaml.load(open(opt.config,'r',encoding='utf-8'),Loader=yaml.FullLoader)
-    config['train']['alphabet'] = loadkeyfile(config['train']['key_file'])
+    config['train']['alphabet'] = loadchkeyfile(config['train']['key_file'])
     config['train']['nclass'] = len(config['train']['alphabet']) + 1
     recog_nin = CRNNInfer(config)
 #     recog_nin = CRNNInferCPU(config)
     if os.path.isdir(path):
         for file in os.listdir(path):
+            if "jpg" not in file:
+                continue
             print(os.path.join(path,file))
             img = Image.open(os.path.join(path,file)).convert('RGB')
             sim_preds_char = recog_nin.recogimg(img)
+            with open(savetxt, "a") as f:
+                f.write(os.path.join(path, file) + "||" + sim_preds_char+"\n")
+
+            # savename = sim_preds_char+".jpg"
+            # shutil.copy(os.path.join(path,file), file)
             print(sim_preds_char)
-            shutil.copy(os.path.join(path,file),'./img.jpg')
 #             break
     else:
         img = Image.open(path).convert('RGB')
@@ -125,8 +152,12 @@ def demo(opt):
         
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, default='./11.jpg', help='DDP parameter, do not modify')
-    parser.add_argument('--gpuid', type=str, default='2', help='DDP parameter, do not modify')
-    parser.add_argument('--config', type=str, default='./config/SVTR-T-Ch-repstn.yaml', help='DDP parameter, do not modify')
+    parser.add_argument('--path', type=str, default='/code/wujilong/code/OCR-TextRecog/testpic', help='DDP parameter, do not modify')
+    parser.add_argument('--gpuid', type=str, default='0', help='DDP parameter, do not modify')
+    # parser.add_argument('--config', type=str, default='./config/SVTR-T-Ch-stn.yaml', help='DDP parameter, do not modify')
+    parser.add_argument('--config', type=str, default='./config/mobilev3CRNN.yaml',
+                        help='DDP parameter, do not modify')
     opt = parser.parse_args()
     demo(opt)
+    # img = cv2.imread("/code/wangshiyuan02/data/ocr/buslic/companyname/errorinfer2/aama.jpg")
+    # h,w = img.shape[:2]
